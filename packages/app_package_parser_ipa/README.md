be67fcd6bbcc2a23860691e6c49b4c4a9bff77af
LiJianying <lijy91@foxmail.com>
2021-12-17 18:10:53 +0800

    Release app_package_parser_ipa v0.0.3

---
+++ packages/app_package_parser_ipa/README.md
@@ -0,0 +1,23 @@
+# app_package_parser_ipa
+
+Parse ipa package.
+
+[![pub version][pub-image]][pub-url]
+
+[pub-image]: https://img.shields.io/pub/v/app_package_parser_ipa.svg
+[pub-url]: https://pub.dev/packages/app_package_parser_ipa
+
+## Quick Start
+
+### Installation
+
+```yaml
+dependencies:
+  app_package_parser_ipa: ^0.0.3
+```
+
+## Usage
+
+```dart
+import 'package:app_package_parser_ipa/app_package_parser_ipa.dart';
+```
---
+++ packages/app_package_parser_ipa/lib/src/app_package_parser_ipa.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:app_package_parser/app_package_parser.dart';
+import 'package:archive/archive.dart';
+import 'package:archive/archive_io.dart';
+import 'package:xml/xml.dart';
+
+class AppPackageParserIpa extends AppPackageParser {
+  @override
+  String get name => 'ipa';
+
+  _handleElem(XmlElement el) {
+    switch (el.name.local) {
+      case 'string':
+        return el.text;
+      case 'real':
+        return double.parse(el.text);
+      case 'integer':
+        return int.parse(el.text);
+      case 'true':
+        return true;
+      case 'false':
+        return false;
+      case 'date':
+        return DateTime.parse(el.text);
+    }
+  }
+
+  @override
+  Future<AppPackage> parse(File file) async {
+    final ipaBytes = file.readAsBytesSync();
+    final archive = ZipDecoder().decodeBytes(ipaBytes);
+
+    String xmlString = '';
+    for (final item in archive) {
+      if (item.isFile && item.name.endsWith('.app/Info.plist')) {
+        final data = item.content as List<int>;
+        xmlString = utf8.decode(data);
+        break;
+      }
+    }
+
+    XmlDocument xmlDoc = XmlDocument.parse(xmlString);
+    XmlElement elPlist = xmlDoc.getElement('plist')!;
+    XmlElement elDict = elPlist.getElement('dict')!;
+
+    List<String> keys = elDict.childElements
+        .where((e) => e.name.local == 'key')
+        .map((e) => e.text)
+        .toList();
+    List<dynamic> values = elDict.childElements
+        .where((e) => e.name.local != 'key')
+        .map(_handleElem)
+        .toList();
+    Map dict = Map.fromIterables(keys, values);
+
+    AppPackage appPackage = AppPackage(
+      platform: 'ios',
+      identifier: dict['CFBundleIdentifier'],
+      name: dict['CFBundleDisplayName'],
+      version: dict['CFBundleShortVersionString'],
+      buildNumber: int.parse(dict['CFBundleVersion']),
+    );
+    return appPackage;
+  }
+}
