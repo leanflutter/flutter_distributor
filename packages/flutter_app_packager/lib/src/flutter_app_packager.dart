6d477ce28b8dbd2a1a8ea488d9e7d68b91b60baa
LiJianying <lijy91@foxmail.com>
2021-12-19 00:15:56 +0800

    Release flutter_app_packager v0.0.6

--- packages/flutter_app_packager/lib/src/flutter_app_packager.dart
+++ packages/flutter_app_packager/lib/src/flutter_app_packager.dart
@@ -6,6 +6,7 @@ import 'package:app_package_maker_apk/app_package_maker_apk.dart';
 import 'package:app_package_maker_deb/app_package_maker_deb.dart';
 import 'package:app_package_maker_dmg/app_package_maker_dmg.dart';
 import 'package:app_package_maker_exe/app_package_maker_exe.dart';
+import 'package:app_package_maker_ipa/app_package_maker_ipa.dart';
 import 'package:app_package_maker_zip/app_package_maker_zip.dart';
 
 class FlutterAppPackager {
@@ -15,31 +16,26 @@ class FlutterAppPackager {
     AppPackageMakerDeb(),
     AppPackageMakerDmg(),
     AppPackageMakerExe(),
-    AppPackageMakerZip(),
+    AppPackageMakerIpa(),
+    AppPackageMakerZip('linux'),
+    AppPackageMakerZip('macos'),
+    AppPackageMakerZip('windows'),
   ];
 
-  Future<MakeResult> package({
-    required String appName,
-    required String appVersion,
-    required Directory appDirectory,
-    required String targetPlatform,
-    required String target,
+  Future<MakeResult> package(
+    Directory appDirectory, {
     required Directory outputDirectory,
+    required String platform,
+    String? flavor,
+    required String target,
   }) async {
     AppPackageMaker maker = _makers.firstWhere(
-      (e) => e.name == target,
+      (e) => e.platform == platform && e.name == target,
     );
-    MakeConfig makeConfig = MakeConfig(
-      appName: appName,
-      appBuildNumber: int.parse(appVersion.split('+').last),
-      appVersion: appVersion.split('+').first,
+    return await maker.make(
+      appDirectory,
       outputDirectory: outputDirectory,
+      flavor: flavor,
     );
-    MakeResult makeResult = await maker.make(
-      appDirectory: appDirectory,
-      targetPlatform: targetPlatform,
-      makeConfig: makeConfig,
-    );
-    return makeResult;
   }
 }
