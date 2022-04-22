21cb5725fc6bf6ccb8b971fbbba17e73c168d75d
Kingkor Roy Tirtho <krtirtho@gmail.com>
2022-04-22 21:46:37 +0600

    AppImage package maker created added new maker to flutter_app_packager updated example to show AppImage package format usage

---
+++ packages/app_package_maker_appimage/README.md
@@ -0,0 +1,12 @@
+# app_package_maker_deb
+
+Create an AppImage for your flutter linux application.
+
+[![pub version][pub-image]][pub-url]
+
+[pub-image]: https://img.shields.io/pub/v/app_package_maker_appimage.svg
+[pub-url]: https://pub.dev/packages/app_package_maker_appimage
+
+---
+
+Part of [flutter_distributor](https://github.com/leanflutter/flutter_distributor), See also https://distributor.leanflutter.org
---
+++ packages/app_package_maker_appimage/lib/src/app_package_maker_appimage.dart
@@ -0,0 +1,79 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:app_package_maker/app_package_maker.dart';
+import 'make_appimage_config.dart';
+import 'package:path/path.dart' as path;
+
+class AppPackageMakerAppImage extends AppPackageMaker {
+  String get name => 'appimage';
+  String get platform => 'linux';
+  String get packageFormat => 'appimage';
+
+  bool get isSupportedOnCurrentPlatform => Platform.isLinux;
+
+  @override
+  Future<MakeConfig> loadMakeConfig(
+    Directory outputDirectory,
+    Map<String, dynamic>? makeArguments,
+  ) async {
+    MakeConfig baseMakeConfig = await super.loadMakeConfig(
+      outputDirectory,
+      makeArguments,
+    );
+    final map = loadMakeConfigYaml('linux/packaging/appimage/make_config.yaml');
+    return MakeAppImageConfig.fromJson(map).copyWith(baseMakeConfig);
+  }
+
+  @override
+  Future<MakeResult> make(
+    Directory appDirectory, {
+    required Directory outputDirectory,
+    Map<String, dynamic>? makeArguments,
+    void Function(List<int> data)? onProcessStdOut,
+    void Function(List<int> data)? onProcessStdErr,
+  }) async {
+    MakeAppImageConfig makeConfig = await loadMakeConfig(
+      outputDirectory,
+      makeArguments,
+    ) as MakeAppImageConfig;
+
+    final configFile = File("AppImageBuilder.yml");
+    final outputFile =
+        File("${makeConfig.appName}-${makeConfig.appVersion}-x86_64.AppImage");
+    final appDir = Directory("AppDir");
+    final buildDir = Directory("appimage-build");
+
+    if (!configFile.existsSync()) configFile.createSync();
+    // removing already used AppDir & appimage-build directories
+    configFile.writeAsStringSync(jsonEncode(makeConfig.toJson()));
+
+    final process = await Process.start('appimage-builder', [
+      "--recipe",
+      configFile.path,
+    ]);
+
+    process.stdout.listen(onProcessStdOut);
+    process.stderr.listen(onProcessStdErr);
+
+    int exitCode = await process.exitCode;
+    if (exitCode != 0) {
+      throw MakeError();
+    }
+
+    // delete the config file & cleaning stuff
+    await configFile.delete();
+    await appDir.delete(recursive: true);
+    await buildDir.delete(recursive: true);
+
+    // move the output file to outputDirectory
+    await outputFile.rename(
+      path.join(
+        outputDirectory.path,
+        makeConfig.appVersion.toString(),
+        "${makeConfig.appName}-${makeConfig.appVersion}-$platform.AppImage",
+      ),
+    );
+
+    return MakeResult(makeConfig);
+  }
+}
