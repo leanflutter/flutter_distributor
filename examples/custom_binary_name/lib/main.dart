7f67107dec2e5e82acffa7e9f4b574fe222dda1a
李健莹 <lijy91@foxmail.com>
2023-06-04 10:55:19 +0800

    chore: update lint workflow & bump melos to 3.1.0

--- examples/custom_binary_name/lib/main.dart
+++ examples/custom_binary_name/lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
