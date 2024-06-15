63d1f91de1bcbb2f8eca6aa03226bbcbc3a2ce9f
李健莹 <lijy91@foxmail.com>
2024-06-15 20:52:34 +0800

    wip: multiple_flavors example support macos part1

---
+++ examples/multiple_flavors/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import package_info_plus
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+}
