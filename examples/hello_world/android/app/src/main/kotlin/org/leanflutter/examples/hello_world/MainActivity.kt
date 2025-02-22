d23ea198274b2f2e14ff2ffb82c160fe44f023d8
李健莹 <lijy91@foxmail.com>
2025-02-22 10:28:10 +0800

    Update Android project configuration and dependencies

--- examples/hello_world/android/app/src/main/kotlin/org/leanflutter/examples/hello_world/MainActivity.kt
+++ examples/hello_world/android/app/src/main/kotlin/org/leanflutter/examples/hello_world/MainActivity.kt
@@ -2,5 +2,4 @@ package org.leanflutter.examples.hello_world
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
